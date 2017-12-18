e61df5faa1120a238d68f6ea5e27465102051d92
VyachevPalmas <gfgvggfg@gmail.com>
2017-12-18 20:37:53 +0300

    09

---
+++ 08. functions/bolchisl.js
@@ -0,0 +1,81 @@
+var sleep = require('system-sleep');
+
+console.log("Первое:");
+
+var first= parseInt(require("readline-sync").question());
+
+console.log("Второе:");
+var second= parseInt(require("readline-sync").question());
+
+
+
+
+
+function bolchisl(first,second){
+    if(first >= second){
+        console.log("Больше "+ first);
+        console.log();
+    }
+    else{
+        console.log("больше "+ second);
+        console.log();
+    }
+}
+bolchisl(first,second);
+sleep(600);
+umnojenie(first, second);
+
+
+
+function umnojenie(first, second){
+    var result = first * second ;
+    console.log("a*b="+result);
+
+}
+console.log();
+console.log();
+console.log();
+console.log();
+console.log("Саня верни сотку!");
+final();
+console.log("Продам гараж:");
+console.log("7-916-666-13-37");
+final();
+console.log("А теперь го поиграем!");
+console.log("")
+
+
+
+function final(){
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+}
